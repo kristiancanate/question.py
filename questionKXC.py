5984fbdccbf22d6e68f68c30de5fa014f20d3c56
csPython <cspython@UAM-iMAc-AD25TF04NGF1J-16.local>
2018-12-10 10:35:56 -0500

    initialized condition and added introduction to user

---
+++ questionKXC.py
@@ -0,0 +1,8 @@
+answered = False
+
+print ("Hello, and welcome to my one question quiz.")
+print ("In this program, you will be asked one question and given four options")
+print ("to choose from.")
+print ("To answer, you will enter the number associated to the option.")
+print ("When you are ready to answer the question, hit enter.")
+input ("")
