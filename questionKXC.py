7eaef697d2d07659349b99f91c3a41eeb3a0b48b
csPython <cspython@UAM-iMAc-AD25TF04NGF1J-16.local>
2018-12-14 09:44:52 -0500

    initialized variable and added fully functional while loop

--- questionKXC.py
+++ questionKXC.py
@@ -1,4 +1,5 @@
 answered = False
+answer = int (0)
 
 print ("Hello, and welcome to my one question quiz.")
 print ("In this program, you will be asked one question and given four options")
@@ -6,3 +7,22 @@ print ("to choose from.")
 print ("To answer, you will enter the number associated to the option.")
 print ("When you are ready to answer the question, hit enter.")
 input ("")
+
+while answered == False:
+    try:
+        print ("QUESTION")
+        print ("OPTION 1")
+        print ("OPTION 2")
+        print ("OPTION 3")
+        print ("OPTION 4")
+        answer = int (input (""))
+        if answer == 2:
+            print ("You are correct!")
+            answered = True
+        elif 0 < answer < 5:
+            print ("You are wrong.")
+            answered = True
+        else:
+            print ("ERROR")
+    except ValueError:
+        print ("ERROR")
